7dd08362e638b66d643d2ff3ab277269527097a0
Mostafa Touny <mostafatouny@protonmail.com>
2024-08-05 08:50:19 +0300

    Mon Aug  5 08:50:17 AM EEST 2024

--- auto-push.sh
+++ auto-push.sh
@@ -11,7 +11,7 @@ cd public
 
 sed -i 's/href="mailto:mostafatouny%3cat%3eproton%3cdot%3eme"/onclick="alert(String.fromCharCode('109','111','115','116','97','102','97','116','111','117','110','121','64','112','114','111','116','111','110','46','109','101'))";/' index.html
 
-sed -i 's/href="https://t.me/mostafatouny"/onclick="alert(String.fromCharCode(116, 46, 109, 101, 47, 109, 111, 115, 116, 97, 102, 97, 116, 111, 117, 110, 121))";/' index.html
+sed -i 's/href="https://t.me/mostafatouny"/onclick="alert(String.fromCharCode('116', '46', '109', '101', '47', '109', '111', '115', '116', '97', '102', '97', '116', '111', '117', '110', '121'))";/' index.html
 
 # git add .
 # git commit -m "$currentDate"
