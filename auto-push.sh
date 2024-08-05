85afa853db1d73a1f1e63ea4466bf8002f69f853
Mostafa Touny <mostafatouny@protonmail.com>
2024-08-05 08:55:15 +0300

    Mon Aug  5 08:55:14 AM EEST 2024

--- auto-push.sh
+++ auto-push.sh
@@ -9,10 +9,12 @@ git push origin master
 hugo
 cd public
 
+# email as unicode values
 sed -i 's/href="mailto:mostafatouny%3cat%3eproton%3cdot%3eme"/onclick="alert(String.fromCharCode('109','111','115','116','97','102','97','116','111','117','110','121','64','112','114','111','116','111','110','46','109','101'))";/' index.html
 
+# telegram as unicode values
 sed -i 's/href="https:\/\/t.me\/mostafatouny"/onclick="alert(String.fromCharCode('116', '46', '109', '101', '47', '109', '111', '115', '116', '97', '102', '97', '116', '111', '117', '110', '121'))";/' index.html
 
-# git add .
-# git commit -m "$currentDate"
-# git push origin master
+git add .
+git commit -m "$currentDate"
+git push origin master
